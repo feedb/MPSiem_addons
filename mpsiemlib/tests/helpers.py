989767f3faff32ff80d6aec62f3a3f977be897a0
nikolaiav <nikolaiav@ya.ru>
2021-08-27 01:50:58 +0300

    add sphinx, autodoc

--- mpsiemlib/tests/helpers.py
+++ mpsiemlib/tests/helpers.py
@@ -2,7 +2,7 @@ from random import choice
 from string import ascii_uppercase, ascii_lowercase
 
 def gen_uppercase_string(strlen: int) -> str:
-    return (''.join(choice(ascii_uppercase) for i in range(strlen)))
+    return ('unittest_' + ''.join(choice(ascii_uppercase) for i in range(strlen)))
 
 def gen_lowercase_string(strlen: int) -> str:
-    return (''.join(choice(ascii_lowercase) for i in range(strlen)))
+    return ('unittest_' + ''.join(choice(ascii_lowercase) for i in range(strlen)))
